9bfc366b36d5c0b3bd974b64a66684e9bf3852eb
Angie <arco527@gmail.com>
2019-04-03 18:56:42 -0500

    Form_for and strong parameters

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,37 @@
 class StudentsController < ApplicationController
 
   def index
-
+    @students = Student.all
   end
 
-  def welcome
-
+  def new
+    @student = Student.new
+    @student2 = Student.first
   end
 
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+  def create
+    student = Student.new(students_params)
+    student.estado = true
+    if student.save
+      redirect_to '/students'
+    end
+  end
+
+  private
+  def students_params
+    params.require(:student).permit(:nombre,:apellido,:edad,:correo)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
 
- root 'students#welcome'
+ root 'students#index'
 
  get '/students', to: "students#index"
-
+ get '/students/new', to: "students#new"
+ get '/students/:id', to: "students#show"
+ get '/students/:id/edit', to: "students#edit"
+ put '/students/:id/' , to: "students#update"
+ delete '/students/:id/' , to: "students#destroy"
+ post '/students', to: "students#create"
 end
